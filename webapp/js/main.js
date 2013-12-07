128181ab4737fa89ccb1c9b78aa694a40f934085
Mathieu Laurent <mathieu.laurent@gmail.com>
2013-12-07 23:06:01 +0100

    Load Jquery without requirejs - To avoid error with not amd jquery plugins (like bootstrap.js, ... )

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,7 +1,12 @@
+// Load jQuery without requireJS (more compatible with jquery plugins not amd)
+define('jquery', [], function() {
+  return jQuery;
+});
+
 // Require.js allows to configure shortcut alias
 require.config({
   paths: {
-    jquery: 'vendor/jquery/jquery-2.1.0-beta1',
+    // jquery: 'vendor/jquery/jquery-2.1.0-beta1',
     underscore: 'vendor/underscore/underscore-min',
     backbone: 'vendor/backbone/backbone-min',
     text: 'vendor/require/text',
