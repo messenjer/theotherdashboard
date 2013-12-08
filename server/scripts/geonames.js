3599533bc0709257bc21a06a655452183701d2cc
Mathieu Laurent <mathieu.laurent@gmail.com>
2013-12-08 21:18:37 +0100

    Use access.secret

--- server/scripts/geonames.js
+++ server/scripts/geonames.js
@@ -1,12 +1,15 @@
 var unirest = require('unirest');
 var urlAPI = 'http://api.geonames.org/countryInfoJSON';
 
+var access = require('../access.secret')
+var username = access.geonames.user;
+
 var Request = unirest.get(urlAPI)
   .headers({ 'Accept': 'application/json' })
   .encoding('utf-8')
   .query({
     lang : 'fr',
-    username : 'messenjer'
+    username : username
   })
   .end(function (response) {
     console.log(response.body);
